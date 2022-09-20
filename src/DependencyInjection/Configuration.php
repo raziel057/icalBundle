3edf7995e0e063fdab1b1dfe65f3766e9eced9c0
Thomas Lallement <thomas.lallement@9online.fr>
2022-09-20 11:38:21 +0200

    Fix creation of TreeBuilder

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('welp_ical');
+        $treeBuilder = new TreeBuilder('welp_ical');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
